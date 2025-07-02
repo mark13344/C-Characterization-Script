71055e8e194b661f4be78c190f3818b3ae9c36e1
mark13344 <mark.collins.20@us.af.mil>
2025-07-01 21:33:55 -0500

    Add files via upload

---
+++ CharacterizerLib/CharacterizerLib/CharacterizationDeployment.ps1
@@ -0,0 +1,2 @@
+﻿add-type -path C:\Users\1mark\Desktop\C-Characterization-Script-main\CharacterizerLib\CharacterizerLib\obj\Debug\CharacterizerLib.dll
+$test = New-Object CharacterizerLib.Characterizer+ProcessInfo
