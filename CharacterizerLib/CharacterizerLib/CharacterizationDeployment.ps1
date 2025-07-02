2617250e742ece32d1f2863719fd17bd640cb246
mark13344 <mark.collins.20@us.af.mil>
2025-07-01 21:33:33 -0500

    Delete CharacterizerLib/CharacterizerLib/CharacterizationDeployment.ps1

--- CharacterizerLib/CharacterizerLib/CharacterizationDeployment.ps1
+++
@@ -1,5 +0,0 @@
-﻿$code = get-content .\Characterizer.cs -Raw
-Add-type -TypeDefinition $code -ReferencedAssemblies "System.Management.dll"
-Add-Type -AssemblyName System.Security
-Add-Type -TypeDefinition $code -Language CSharp
-$machine = New-Object Characterizer+ProcessInfo
